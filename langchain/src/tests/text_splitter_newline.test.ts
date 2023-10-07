076407a4a4cfc9f1694c3272c1fec2c01a9f0ee8
tonisives <tonisives@gmail.com>
2023-10-07 14:55:31 +0700

    add text splitter newline

---
+++ langchain/src/tests/text_splitter_newline.test.ts
@@ -0,0 +1,59 @@
+import { expect, test } from "@jest/globals";
+import { Document } from "../document.js";
+import fs from "fs"
+import { TextSplitterNewLine } from "../text_splitter_newline.js";
+
+test("overlap and lines sol", async () => {
+  const splitter = new TextSplitterNewLine({
+    chunkSize: 650,
+    chunkOverlap: 300
+  });
+  let text = fs.readFileSync("./src/tests/tonis/sample.sol").toString()
+
+  const docs = await splitter.createDocuments([text], undefined, undefined);
+
+  printResultToFile("sample.sol", docs)
+
+  for (let i = 1; i < docs.length; i++) {
+    let prev = docs[i - 1]
+    let curr = docs[i]
+
+    expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(curr.metadata.loc.lines.from)
+    let prevLastLine = prev.pageContent.split("\n").at(-1)!
+    expect(curr.pageContent.split("\n").some(it => it.startsWith(prevLastLine))).toBe(true)
+  }
+
+  expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length)
+});
+
+test("overlap and lines md", async () => {
+  const splitter = new TextSplitterNewLine({
+    chunkSize: 550,
+    chunkOverlap: 200
+  });
+
+  let text = fs.readFileSync("./src/tests/tonis/sample.md").toString()
+  const docs = await splitter.createDocuments([text], undefined, undefined);
+  printResultToFile("sample.md", docs)
+
+  for (let i = 1; i < docs.length; i++) {
+    let prev = docs[i - 1]
+    let curr = docs[i]
+
+    expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(curr.metadata.loc.lines.from)
+  }
+
+  expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length)
+});
+
+const printResultToFile = (fileName: string, docs: Document[]) => {
+  let file = ""
+  for (const doc of docs) {
+    let metadata = doc.metadata
+    let content = doc.pageContent
+    file += `${JSON.stringify(metadata, null, 2)}\n${content}\n`
+  }
+
+  fs.mkdirSync("./src/tests/tonis/results", { recursive: true })
+  fs.writeFileSync(`./src/tests/tonis/results/${fileName}.txt`, file)
+}
---
+++ langchain/src/text_splitter_newline.ts
@@ -0,0 +1,128 @@
+import { RecursiveCharacterTextSplitterParams, TextSplitter, TextSplitterChunkHeaderOptions } from "./text_splitter.js";
+import { Document } from "./document.js";
+
+export type TextSplitterNewLineParams = Pick<RecursiveCharacterTextSplitterParams, "chunkSize" | "chunkOverlap">;
+
+export class TextSplitterNewLine
+  extends TextSplitter
+  implements TextSplitterNewLineParams {
+  constructor(fields?: Partial<TextSplitterNewLineParams>) {
+    super(fields);
+  }
+
+  async createDocuments(
+    texts: string[],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    metadatas: Record<string, any>[] = [],
+    // not implemented
+    _chunkHeaderOptions: TextSplitterChunkHeaderOptions = {}
+  ): Promise<Document[]> {
+    let builder = [] as Document[][]
+
+    for (const text of texts) {
+      let docs = await this._splitText(text)
+      builder.push(docs.map(it => ({
+        ...it,
+        metadata: {
+          ...it.metadata,
+          ...metadatas[builder.length]
+        }
+      })))
+    }
+
+    return builder.flat()
+  }
+
+  async splitText(text: string): Promise<string[]> {
+    let docs = await this._splitText(text)
+    return docs.map(doc => doc.pageContent)
+  }
+
+  private async _splitText(text: string): Promise<Document[]> {
+    const addDoc = (builder: Document[], pageContent: string[], lineCounter: number) => {
+      builder.push({
+        pageContent: pageContent.join("\n"),
+        metadata: {
+          loc: {
+            lines: {
+              from: lineCounter - pageContent.length,
+              to: lineCounter - 1
+            }
+          }
+        }
+      })
+    }
+
+    const getDocsFromText = (text: string) => {
+      let builder: Document[] = []
+      let lines = text.split("\n")
+
+      let pageContent = [] as string[]
+      let lineCounter = 0
+
+      for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        lineCounter++
+
+        let currentPageContent = pageContent.join("\n")
+        let lineWillFillChunk = currentPageContent.length + line.length > (this.chunkSize - this.chunkOverlap)
+
+        // if line + overlap is longer than the chunk, it will be added in next loop with overflown size
+        if (lineWillFillChunk && pageContent.length > 0) {
+          if (currentPageContent.trim().length > 0) addDoc(builder, pageContent, lineCounter)
+          pageContent = []
+        }
+
+        pageContent.push(line)
+
+        if (i === lines.length - 1) {
+          lineCounter++
+          addDoc(builder, pageContent, lineCounter)
+        }
+      }
+
+      return builder
+    }
+
+    let docs = getDocsFromText(text)
+    let withOverlap = this.addOverlapFromPreviousChunks(docs);
+
+    return withOverlap
+  }
+
+  private addOverlapFromPreviousChunks(builder: Document[]) {
+    if (builder.length <= 1) return builder
+
+    for (let i = 1; i < builder.length; i++) {
+      let prevChunkLines = builder[i - 1].pageContent.split("\n");
+
+      let addedLines = [] as string[]
+
+      for (let j = prevChunkLines.length - 1; j > 0; j--) {
+        let prevLine = prevChunkLines[j];
+        if (addedLines.join("\n").length + prevLine.length > this.chunkOverlap) {
+          break;
+        }
+
+        addedLines.push(prevLine);
+      }
+
+      addedLines = addedLines.reverse();
+
+      builder[i] = {
+        pageContent: `${addedLines.join("\n")}\n${builder[i].pageContent}`,
+        metadata: {
+          ...builder[i].metadata,
+          loc: {
+            lines: {
+              from: builder[i].metadata.loc.lines.from - addedLines.length,
+              to: builder[i].metadata.loc.lines.to
+            }
+          }
+        }
+      }
+    }
+
+    return builder
+  }
+}
