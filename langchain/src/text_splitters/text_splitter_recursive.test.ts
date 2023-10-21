499b2a7cd2407dedc85473248a3ea2e7ecfa16b4
tonisives <tonisives@gmail.com>
2023-10-21 15:10:38 +0700

    refactor

--- langchain/src/text_splitters/text_splitter_recursive.test.ts
+++ langchain/src/text_splitters/text_splitter_recursive.test.ts
@@ -4,116 +4,122 @@ import fs from "fs";
 import { getLengthNoWhitespace, preSplitSol, splitOnSolComments } from "./utils.js";
 import { TextSplitterRecursive } from "./text_splitter_recursive.js";
 
-test("splits full contract", async () => {
-  const splitter = new TextSplitterRecursive({
-    chunkSize: 550,
-    chunkOverlap: 0,
-    type: "sol",
-  });
-  let text = fs.readFileSync("./src/text_splitters/tests/samples/sample.sol").toString();
+describe("sol", () => {
+  test("splits full contract", async () => {
+    const splitter = new TextSplitterRecursive({
+      chunkSize: 550,
+      chunkOverlap: 0,
+      type: "sol",
+    });
+    let text = fs.readFileSync("./src/text_splitters/tests/samples/sample.sol").toString();
 
-  const docs = await splitter.createDocuments([text], undefined, undefined);
+    const docs = await splitter.createDocuments([text], undefined, undefined);
 
-  printResultToFile("sample.sol", docs, "recursive");
+    printResultToFile("sample.sol", docs, "recursive");
 
-  let fullFile = docs.map(d => d.pageContent).join("")
+    let fullFile = docs.map(d => d.pageContent).join("")
 
-  expect(fullFile).toBe(text)
+    expect(fullFile).toBe(text)
 
-  for (let i = 1; i < docs.length; i++) {
-    let prev = docs[i - 1];
-    let curr = docs[i];
-
-    expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(
-      curr.metadata.loc.lines.from
-    );
-  }
-
-  expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length);
-  verifyMiddleChunksWithCorrectLength(docs, 550, 550 * 0.3);
-});
-
-test("splits long if case", async () => {
-  // verify long block is split correctly on separators (`if`) and the original text is preserved 1:1
-  const splitter = new TextSplitterRecursive({
-    chunkSize: 550,
-    chunkOverlap: 0,
-    type: "sol",
-  });
-  let text = fs.readFileSync("./src/text_splitters/tests/samples/sample-long-if.sol").toString();
-
-  const docs = await splitter.createDocuments([text], undefined, undefined);
-
-  printResultToFile("sample-long-if.sol", docs, "recursive");
-
-  let allLines = ""
-
-  for (let i = 0; i < docs.length; i++) {
-    let curr = docs[i];
-
-    if (i > 0) {
+    for (let i = 1; i < docs.length; i++) {
       let prev = docs[i - 1];
+      let curr = docs[i];
+
       expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(
         curr.metadata.loc.lines.from
       );
     }
 
-    allLines += curr.pageContent
-  }
-
-  expect(allLines).toBe(text)
-
-  expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length);
-  verifyMiddleChunksWithCorrectLength(docs, 550, 550 * 0.2);
-})
-
-test("pre split sol", async () => {
-  let text = fs.readFileSync("./src/text_splitters/tests/samples/sample.sol").toString();
-  let split = preSplitSol(text, 550, 0)
-  let joined = split.join("")
-  expect(joined).toBe(text)
-})
-
-test("split on comments", async () => {
-  let text = fs.readFileSync("./src/text_splitters/tests/samples/sample.sol").toString();
-  let split = splitOnSolComments(text)
-  let joined = split.map(s => s.join("")).join("")
-  expect(joined).toBe(text)
-})
-
-/* test("overlap and lines md", async () => {
-  const splitter = new TextSplitterNewLine({
-    chunkSize: 550,
-    chunkOverlap: 200,
+    expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length);
+    verifyMiddleChunksWithCorrectLength(docs, 550, 550 * 0.3);
   });
 
-  let text = fs.readFileSync("./src/tests/samples/sample.md").toString();
-  const docs = await splitter.createDocuments([text], undefined, undefined);
-  printResultToFile("sample.md", docs);
+  test("splits long if case", async () => {
+    // verify long block is split correctly on separators (`if`) and the original text is preserved 1:1
+    const splitter = new TextSplitterRecursive({
+      chunkSize: 550,
+      chunkOverlap: 0,
+      type: "sol",
+    });
+    let text = fs.readFileSync("./src/text_splitters/tests/samples/sample-long-if.sol").toString();
 
-  for (let i = 1; i < docs.length; i++) {
-    let prev = docs[i - 1];
-    let curr = docs[i];
+    const docs = await splitter.createDocuments([text], undefined, undefined);
 
-    expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(
-      curr.metadata.loc.lines.from
-    );
-  }
+    printResultToFile("sample-long-if.sol", docs, "recursive");
 
-  expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length);
-});
+    let allLines = ""
 
-test("adds a slice of line if overlap line too long", async () => {
-  const splitter = new TextSplitterNewLine({
-    chunkSize: 550,
-    chunkOverlap: 200,
+    for (let i = 0; i < docs.length; i++) {
+      let curr = docs[i];
+
+      if (i > 0) {
+        let prev = docs[i - 1];
+        expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(
+          curr.metadata.loc.lines.from
+        );
+      }
+
+      allLines += curr.pageContent
+    }
+
+    expect(allLines).toBe(text)
+
+    expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length);
+    verifyMiddleChunksWithCorrectLength(docs, 550, 550 * 0.2);
+  })
+
+  test("pre split sol", async () => {
+    let text = fs.readFileSync("./src/text_splitters/tests/samples/sample.sol").toString();
+    let split = preSplitSol(text, 550, 0)
+    let joined = split.join("")
+    expect(joined).toBe(text)
+  })
+
+  test("split on comments", async () => {
+    let text = fs.readFileSync("./src/text_splitters/tests/samples/sample.sol").toString();
+    let split = splitOnSolComments(text)
+    let joined = split.map(s => s.join("")).join("")
+    expect(joined).toBe(text)
+  })
+})
+
+/* describe("md", () => {
+  test("overlap and lines md", async () => {
+    const splitter = new TextSplitterNewLine({
+      chunkSize: 550,
+      chunkOverlap: 200,
+    });
+
+    let text = fs.readFileSync("./src/tests/samples/sample.md").toString();
+    const docs = await splitter.createDocuments([text], undefined, undefined);
+    printResultToFile("sample.md", docs);
+
+    for (let i = 1; i < docs.length; i++) {
+      let prev = docs[i - 1];
+      let curr = docs[i];
+
+      expect(prev.metadata.loc.lines.from).toBeLessThanOrEqual(
+        curr.metadata.loc.lines.from
+      );
+    }
+
+    expect(docs.at(-1)?.metadata.loc.lines.to).toBe(text.split("\n").length);
   });
 
-  let text = fs.readFileSync("./src/tests/samples/sample.md").toString();
-  const docs = await splitter.createDocuments([text], undefined, undefined);
+  test("adds a slice of line if overlap line too long", async () => {
+    const splitter = new TextSplitterNewLine({
+      chunkSize: 550,
+      chunkOverlap: 200,
+    });
 
-  verifyMiddleChunksWithCorrectLength(docs, 550);
-}); */
+    let text = fs.readFileSync("./src/tests/samples/sample.md").toString();
+    const docs = await splitter.createDocuments([text], undefined, undefined);
+
+    verifyMiddleChunksWithCorrectLength(docs, 550);
+  });
+
+}) */
+
 
 const verifyMiddleChunksWithCorrectLength = (docs: Document[], chunkSize: number, deviation = 5) => {
   // deviation: on character split needs to be precise(5)
@@ -127,7 +133,7 @@ const verifyMiddleChunksWithCorrectLength = (docs: Document[], chunkSize: number
 
     let length = getLengthNoWhitespace(curr.pageContent.split("\n"));
     let distFromSize = Math.abs(length - chunkSize);
-    
+
     // since we don't match whitespace, then deviation is acceptable
     expect(distFromSize).toBeLessThanOrEqual(deviation);
   }
